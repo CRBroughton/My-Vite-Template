80bae529fb4ee6eb1282a463e8963be9113935b0
CRBroughton <crbroughton@posteo.uk>
2021-10-30 11:04:30 +0100

    installed windicss

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="flex flex-col items-center">
+    <img alt="Vue logo" src="./assets/logo.png" class="vue-logo w-32" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
 </template>
 
 <style>
