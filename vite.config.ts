4e2ae59bccd8799373bc7cb00a2d23cd9e84a64d
CRBroughton <crbroughton@posteo.uk>
2021-10-31 07:19:46 +0000

    added @/ alias for src

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import Components from 'unplugin-vue-components/vite'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS(), Components({  dts: true }),]
+  plugins: [vue(), WindiCSS(), Components({  dts: true }),],
+  resolve:{
+    alias: [
+      { find: '@', replacement: '/src' },
+    ]
+  },
 })
