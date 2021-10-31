55192b46f9473a3b49ee192df9f45947a3c5348d
CRBroughton <crbroughton@posteo.uk>
2021-10-31 07:19:35 +0000

    installed pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useStore } from '@/store/main'
+import { storeToRefs } from 'pinia'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const store = useStore()
+const { count } = storeToRefs(store)
 </script>
 
 <template>
@@ -26,7 +28,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="store.increment">count is: {{ store.count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
