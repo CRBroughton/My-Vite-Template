2f2efac5e128ece5f65361bfd24f078271a00dde
CRBroughton <crbroughton@posteo.uk>
2021-11-03 21:12:36 +0000

    added dynamic page example

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,5 +5,19 @@
     <router-link class="font-bold" to="/about">
       Go to the about page
     </router-link>
+    <input v-model="name" class="text-center w-70" type="text" placeholder="Enter text here to go to a dynamic page" @keydown.enter="go">
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const name = ref('')
+
+const go = () => {
+  if (name.value)
+    router.push(`/users/${encodeURIComponent(name.value)}`)
+}
+</script>
---
+++ src/pages/users/[name].vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Welcome to your dynamic page {{ props.name }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ name: string }>()
+</script>
