6b93c9b7ca3339890c2f4595f4dc326c19495e35
CRBroughton <crbroughton@posteo.uk>
2021-11-07 14:16:28 +0000

    removed component mounting from store test

--- tests/unit/mainStore.spec.ts
+++ tests/unit/mainStore.spec.ts
@@ -1,16 +1,12 @@
 /// <reference types="cypress" />
 
-import { mount } from '@cypress/vue'
 import { useStore } from '@/store/main'
-import HelloWorld from '@/components/HelloWorld.vue'
-
 describe('Counter Store', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
   })
   it('increments the count', () => {
     const counter = useStore()
-    mount(HelloWorld)
     expect(counter.count).to.equal(0)
     counter.increment()
     expect(counter.count).to.equal(1)
