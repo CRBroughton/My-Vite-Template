1919b866fa5a3d1dca4b560f3580745f6603d0ff
CRBroughton <crbroughton@posteo.uk>
2021-11-07 09:41:49 +0000

    improved HelloWorld.spec.ts with pinia example

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -2,6 +2,7 @@
 
 import { mount } from '@cypress/vue'
 import { setActivePinia, createPinia } from 'pinia'
+import { useStore } from '@/store/main'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('Counter Store', () => {
@@ -17,5 +18,12 @@ describe('Counter Store', () => {
     })
 
     cy.get('h1').contains(msg)
+  })
+  it('increments the count', () => {
+    const counter = useStore()
+    mount(HelloWorld)
+    expect(counter.count).to.equal(0)
+    counter.increment()
+    expect(counter.count).to.equal(1)
   })
 })
