85dcc0a30c5fd53fe403280c1f8293f7a3e06cea
CRBroughton <crbroughton@posteo.uk>
2023-08-25 16:47:09 +0100

    feat: :sparkles: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import Vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   test: {
@@ -12,7 +13,58 @@ export default defineConfig({
   server: {
     port: 4000,
   },
-  plugins: [Vue(), Pages(), Components({ dts: true }),
+  plugins: [
+    Vue(),
+    Pages(),
+    Components({ dts: true }),
+    VitePWA({
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+      manifest: {
+        name: '<Application Name>',
+        short_name: '<Application Short Name >',
+        theme_color: '#1976d2',
+        background_color: '#fafafa',
+        display: 'standalone',
+        scope: './',
+        start_url: './',
+        icons: [
+          {
+            src: 'assets/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+          {
+            src: 'assets/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+          {
+            src: 'assets/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+          {
+            src: 'assets/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+          {
+            src: 'assets/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+        ],
+      },
+    }),
     AutoImport({
       // targets to transform
       include: [
